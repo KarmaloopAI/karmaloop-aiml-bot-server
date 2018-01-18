c377723fd58156593c6f4eabef99e428e6141cda
Abhishek Chatterjee <abhishek@gritsa.com>
2018-01-18 14:51:30 +0530

    Windows Build batch file added
    This build.bat file will allow you to compile the server into your local binaries folder.

---
+++ build.bat
@@ -0,0 +1,35 @@
+@echo off
+REM Karmaloop AI (https://www.karmaloop.ai)
+REM This batch script lets you build the Karmaloop AIML Bot Server binaries and place the config and aiml files in the right directories
+
+ECHO Karmaloop AIML Bot Server
+ECHO Starting build process...
+ECHO Prerequisites:
+ECHO .NET Framework 4.5 and 4.0
+
+REM Initialize configuration variables.
+SET binariesDir=.\binaries
+SET serverDir=.\KarmaloopAIMLBotServer
+SET solutionName=KarmaloopAIMLBotServer.sln
+SET configDir=.\config
+SET aimlDir=.\aiml
+SET msBuildDir=%WINDIR%\Microsoft.NET\Framework\v4.0.30319
+
+REM Fix up directories, clean up and create what is needed
+RD %binariesDir% /S /Q
+RD %serverDir%\Bin\Release  /S /Q
+MD %binariesDir%
+MD %binariesDir%\config
+MD %binariesDir%\aiml
+
+REM Start the build process
+CALL %msBuildDir%\msbuild.exe  %solutionName% /p:Configuration=Release /l:FileLogger,Microsoft.Build.Engine;logfile=Manual_MSBuild_ReleaseVersion_LOG.log
+
+REM Copy the freshly built binaries to the binaries directory and then copy the config and aiml files. These files are needed for the server to initialize.
+XCOPY .\%serverDir%\Bin\Release\*.* %binariesDir%\
+XCOPY %configDir%\*.* %binariesDir%\config\
+XCOPY %aimlDir%\*.* %binariesDir%\aiml\
+
+ECHO
+ECHO
+ECHO If you did not get the binaries built into the binaries folder, or if there was an error, please check you have the right .NET Framework installed.
